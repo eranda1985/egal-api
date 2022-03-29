ed6c62c5cee6359be84855daac5a6ad0d5fead49
(Randy) Eranda Galhenage <eranda1985@yahoo.com>
2022-03-29 11:33:59 +1030

    feat: enable swagger in production

--- Program.cs
+++ Program.cs
@@ -28,7 +28,7 @@ builder.Services.AddTransient<ILinearService, LinearService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
 {
     app.UseSwagger();
     app.UseSwaggerUI();
